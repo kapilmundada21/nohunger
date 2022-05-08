f708dcf98bf25eae4a89b4bf79e45a1063f7c297
kapilmundada <kapilmundada21@gmail.com>
2022-05-08 16:23:48 +0530

    live changes

--- login.php
+++ login.php
@@ -56,7 +56,7 @@
 					</div>
 
 					<div class="text-center p-t-136">
-						<a class="txt2" href="registration.html">
+						<a class="txt2" href="registration.php">
 							Create your Account
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
--- other/footer.php
+++ other/footer.php
@@ -5,14 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
-        .site-footer{
-            padding: 100px;
-            background-color: #2B2E33;
-            color: white;
+        .site-footer {
+          padding: 100px;
+          background-color: #2B2E33;
+          color: white;
         }
-        .site-footer a{
+        .site-footer a {
             color: lightgray;
             text-decoration: none;
+        }
+        @media only screen and (max-width: 400px) {
+          .site-footer {
+            padding: 5%;
+            padding-top: 50px;
+            text-align: center;
+          }
+          .dropdown {
+            margin-top: 20px;
+          }
         }
     </style>
 </head>
